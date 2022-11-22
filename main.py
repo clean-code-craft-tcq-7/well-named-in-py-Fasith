0d826d21553ebb3f5d5f08e4405074c040d4b4ac
Fasith Ali K Mohamed (MS/EXV1-XC) <fak2kor@BAND-C-000XM.ban.apac.bosch.com>
2022-11-22 12:01:43 +0530

    autopep8 refactor

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tests import test_number_to_pair, test_pair_to_number
+from utils import get_color_manual
 
 if __name__ == '__main__':
-  test_number_to_pair(4, 'White', 'Brown')
-  test_number_to_pair(5, 'White', 'Slate')
-  test_pair_to_number('Black', 'Orange', 12)
-  test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
-  print('Done :)')
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    color_manual = get_color_manual()
+    print(color_manual)
--- utils.py
+++ utils.py
@@ -1,2 +1,12 @@
-def color_pair_to_string(major_color, minor_color):
-  return f'{major_color} {minor_color}'
+from config import MAJOR_COLORS, MINOR_COLORS
+from prettytable import PrettyTable
+
+
+def get_color_manual():
+    pair_number = 0
+    table = PrettyTable(['MAJOR COLOR', 'MINOR COLOR', 'PAIR NUMBER'])
+    for major_color in MAJOR_COLORS:
+        for minor_color in MINOR_COLORS:
+            pair_number += 1
+            table.add_row([major_color, minor_color, pair_number])
+    return table
